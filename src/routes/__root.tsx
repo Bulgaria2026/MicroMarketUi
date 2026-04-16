88a15a340ac75bd174e475ecf7eaf6f7fea942c5
Cuerillius <cyrill_weber@hotmail.com>
2026-04-16 13:50:02 +0200

    add tanstack router and sign in/up page

---
+++ src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 flex gap-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>{" "}
+      <Link to="/signin" className="[&.active]:font-bold">
+        Sign in
+      </Link>
+      <Link to="/signup" className="[&.active]:font-bold">
+        Sign up
+      </Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout, notFoundComponent: () => <div>404 Not Found</div> });
